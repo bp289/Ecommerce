239936db5e055d3407d46f704bf7b1c28bc6e52c
bp289 <birajpanta112@gmail.com>
2023-12-09 16:46:09 +0000

    add products set up

--- frontend/src/components/Products/AddProduct.tsx
+++ frontend/src/components/Products/AddProduct.tsx
@@ -1,14 +1,64 @@
-import { Typography } from "@material-tailwind/react";
-import React from "react";
+import { Card, Input, Typography, Button } from "@material-tailwind/react";
+
+import { useState } from "react";
+
 import Header from "../Header";
 
 type Props = {};
 
 const AddProduct = (props: Props) => {
+  const [name, setName] = useState<string>("");
+  const [file, setFile] = useState<File>();
+  const [price, setPrice] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+
+  async function addProduct() {
+    const formData = new FormData();
+
+    formData.append("file", file!);
+    formData.append("price", price);
+    formData.append("name", name);
+    formData.append("description", description);
+
+    const result = await fetch("http://localhost:8000/api/addproduct", {
+      method: "POST",
+      body: formData,
+    });
+    alert("Data was saved");
+  }
   return (
     <div>
       <Header />
       <Typography variant="h1">Add products</Typography>
+      <Card>
+        <Input
+          label="name"
+          crossOrigin={undefined}
+          type="text"
+          className="form-control"
+          onChange={(e) => setName(e.target.value)}
+        ></Input>
+
+        <Input
+          label="price"
+          crossOrigin={undefined}
+          type="text"
+          className="form-control"
+          onChange={(e) => setPrice(e.target.value)}
+        ></Input>
+        <Input
+          label="description"
+          crossOrigin={undefined}
+          type="text"
+          className="form-control"
+          onChange={(e) => setDescription(e.target.value)}
+        ></Input>
+        <input
+          type="file"
+          onChange={(e) => setFile(e.target.files![0])}
+        ></input>
+        <Button onClick={addProduct}>Add Item</Button>
+      </Card>
     </div>
   );
 };
