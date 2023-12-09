79dcf734bb186e54576286d4dde1c66cd6d59b5e
bp289 <birajpanta112@gmail.com>
2023-12-09 16:25:37 +0000

    add products set up

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('register',[UserController::class, 'register']);
 
 Route::post('signIn', [UserController::class, 'signIn']);
+
+Route::post('addProduct', [ProductController::class,'addProduct']);
